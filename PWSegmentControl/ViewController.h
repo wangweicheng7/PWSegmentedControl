94f5a69de01ddf28d586e549c185ed86fc7cb770
Paul Wang <Paul@hataasahis-MacBook-Pro.local>
2016-01-30 17:11:08 +0800

    v1.0

---
+++ PWSegmentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PWSegmentControl
+//
+//  Created by Paul Wang on 16/1/30.
+//  Copyright © 2016年 Paul Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PWSegmentControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PWSegmentControl
+//
+//  Created by Paul Wang on 16/1/30.
+//  Copyright © 2016年 Paul Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
